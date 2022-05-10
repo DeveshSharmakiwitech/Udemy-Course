1a6206595630d65fd689185fe9893f334dab4100
DeveshSharmakiwitech <devesh.sharma@kiwitech.com>
2022-05-10 12:57:23 +0530

    add

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,15 +1,28 @@
-// const { rejects } = require("assert");
-// const { resolve } = require("uri-js");
+const add=(a,b)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(a+b)
+        },2000)
+    })
+}
 
-const doWorkPromises=new Promise((resolve,rejects)=>{
-    setTimeout(()=>{
-        // resolve([7,4,1])
-        rejects('Things went wrong!')
-    },2000)
-})
+// add(1,2).then((sum)=>{
+//     console.log(sum)
 
-doWorkPromises.then((result)=>{
-    console.log('Success',result)
-}).catch((error)=>{
-    console.log('Error!',error)
+//     add(sum,5).then((sum2)=>{
+//         console.log(sum2)
+//     }).catch((e)=>{
+//         console.log(e)
+//     })
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+add(1,1).then((sum)=>{
+    console.log(sum)
+    return add(sum,4)
+}).then((sum2)=>{
+    console.log(sum2)
+}).catch((e)=>{
+    console.log(e)
 })
