9ded810a8ae97c4a33732b323bc05a435901f9d8
DeveshSharmakiwitech <devesh.sharma@kiwitech.com>
2022-05-12 12:23:56 +0530

    add routers folder

--- task-manager/src/db/models/user.js
+++ task-manager/src/db/models/user.js
@@ -1,14 +1,17 @@
 const mongoose=require('mongoose')
 const validator=require('validator')
+const bcrypt=require('bcryptjs')
 
-const User=mongoose.model('user',{
+const userSchema=new mongoose.Schema({
     name:{
         type:String,
         required:true,
-        trim:true
+        trim:true,
+        
     },
     email:{
         type:String,
+        unique:true,
         required:true,
         trim:true,
         lowercase:true,
@@ -39,5 +42,35 @@ const User=mongoose.model('user',{
         }
     }
 })
+
+userSchema.statics.findByCredentials=async(email,password)=>{
+    const user=await User.findOne({email})
+
+    if(!user){
+        throw new Error('Unable to login')
+    }
+
+    const ismatch=await bcrypt.compare(password, user.password)
+
+    if(!ismatch){
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+//hash the plain text password before saving
+
+userSchema.pre('save', async function(next){
+    const user=this
+
+    if(user.isModified('password')){
+        user.password=await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User=mongoose.model('user',userSchema )
 
 module.exports=User
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,165 +2,41 @@ const express = require('express')
 require('./db/mongoose')
 const User=require('./db/models/user')
 const Task=require('./db/models/task')
+const userrouter=require('./routers/user')
+const taskrouter=require('./routers/task')
 
 const app=express()
 const port=process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users',async(req,res)=>{
-    const user=new User(req.body)
-
-    try{
-        await user.save()
-        res.status(201).send(user)
-    }
-    catch(e)
-{
-    res.status(400).send(e)
-}
-})
-
-
-app.get('/users',async(req,res)=>{
-    try{
-        const user=await User.find({})
-        res.send(user)
-    }
-    catch{
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id',async(req,res)=>{
-    const _id=req.params.id
-
-    try{
-        const user= await User.findById(_id)
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }
-    catch(e){
-        res.status(500).send()
-    }
-})
-
-app.patch('/users/:id',async(req,res)=>{
-    const updates = Object.keys(req.body)
-    const allowedUpdates=['name','email','password','age']
-    const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
-
-    if(!isValidOperation){
-        return res.status(400).send({error:'Invalid updates'})
-    }
-
-    try{
-        const user=await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
-        if(!user){
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    }
-    catch(e){
-        res.status(500).send()
-    }
-})
-
-app.delete('/users/:id',async(req,res)=>{
-    try{
-        const user=await User.findByIdAndDelete(res.params.id)
-        if(!user){
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    }
-    catch(e){
-        res.status(500).send()
-        console.log(e)
-    }
-})
-
-
-app.post('/task',async(req,res)=>{
-    const task=new Task(req.body)
-
-    try{
-       await task.save()
-        res.status(201).send(task)
-    }
-    catch(e){
-        res.status(400).send(e)
-    }
-})
-
-app.get('/tasks',async(req,res)=>{
-
-    try{
-        const task=await Task.find({})
-        res.send(task)
-    }
-    catch(e){
-        res.status(500).send()
-    }
-})
-
-app.get('/tasks/:id',async(req,res)=>{
-    const _id=req.params.id
-
-    try{
-        const task= await Task.findById(_id)
-            if(!task){
-                return res.status(404).send()
-            }
-    
-            res.send(task)
-    }
-    catch(e){
-        res.status(500).send()
-    }
-})
-
-app.patch('/tasks/:id',async(req,res)=>{
-    const updates=Object.keys(req.body)
-    const allowedUpdates=['description','completed']
-    const isValidOperation=updates.every((update)=>allowedUpdates.includes(update))
-
-    if(!isValidOperation){
-        return res.status(400).send({error:'Invalid updates!'})
-    }
-
-    try{
-        const task=await Task.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
-
-        if(!task){
-            return res.status(404).send()
-        }
-        res.send(task)
-    }
-    catch(e){
-        res.status(400).send(e)
-    }
-})
-
-app.delete('/tasks/:id',async(req,res)=>{
-    try{
-        const task = await Task.findByIdAndDelete(req.params.id)
-
-        if(!task){
-            res.status(404).send()
-        }
-
-        res.send(task)
-    }
-    catch(e){
-        res.status(500).send()
-    }
-})
+app.use(userrouter)
+app.use(taskrouter)
 
 app.listen(port,()=>{
     console.log("server is up on port " + port)
 })
+
+const jwt=require('jsonwebtoken')
+
+const myFuntion=async()=>{
+   const token= jwt.sign({ _id:'abc123' },'thisismynewcourse',{expiresIn:'7 days'})
+   console.log(token)
+
+   const data=jwt.verify(token,'thisismynewcourse')
+    console.log(data)
+}
+
+// const bcrypt=require("bcryptjs")
+
+// const myFuntion=async()=>{
+//     const password='Devesh1234'
+//     const hashedPassword=await bcrypt.hash(password,8)
+
+//     console.log(password)
+//     console.log(hashedPassword)
+
+//     const ismatch=await bcrypt.compare('Devesh1234',hashedPassword)
+//     console.log(ismatch)
+// }
+
+myFuntion()
