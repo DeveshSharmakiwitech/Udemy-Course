a309794f9cf5b669e75b03fa9391390f3b5621be
DeveshSharmakiwitech <devesh.sharma@kiwitech.com>
2022-05-18 16:31:04 +0530

    logout page add

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -31,27 +31,37 @@ app.listen(port,()=>{
     console.log("server is up on port " + port)
 })
 
-const jwt=require('jsonwebtoken')
-
-const myFuntion=async()=>{
-   const token= jwt.sign({ _id:'abc123' },'thisismynewcourse',{expiresIn:'7 days'})
-   console.log(token)
-
-   const data=jwt.verify(token,'thisismynewcourse')
-    console.log(data)
+const pet ={
+    name:'Hal'
 }
 
-// const bcrypt=require("bcryptjs")
+pet.toJSON=function(){
+    return {}
+}
+
+console.log(JSON.stringify(pet))
+
+// const jwt=require('jsonwebtoken')
 
 // const myFuntion=async()=>{
-//     const password='Devesh1234'
-//     const hashedPassword=await bcrypt.hash(password,8)
+//    const token= jwt.sign({ _id:'abc123' },'thisismynewcourse',{expiresIn:'7 days'})
+//    console.log(token)
 
-//     console.log(password)
-//     console.log(hashedPassword)
-
-//     const ismatch=await bcrypt.compare('Devesh1234',hashedPassword)
-//     console.log(ismatch)
+//    const data=jwt.verify(token,'thisismynewcourse')
+//     console.log(data)
 // }
 
-myFuntion()
+// // const bcrypt=require("bcryptjs")
+
+// // const myFuntion=async()=>{
+// //     const password='Devesh1234'
+// //     const hashedPassword=await bcrypt.hash(password,8)
+
+// //     console.log(password)
+// //     console.log(hashedPassword)
+
+// //     const ismatch=await bcrypt.compare('Devesh1234',hashedPassword)
+// //     console.log(ismatch)
+// // }
+
+// myFuntion()
