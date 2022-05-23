5ac21b8d4b184fb41b42deb8036798e6ef6f0f44
DeveshSharmakiwitech <devesh.sharma@kiwitech.com>
2022-05-23 16:59:32 +0530

    file uploading

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -24,35 +24,35 @@ const port=process.env.PORT || 3000
 // })
 
 
-const multer = require('multer')
-const upload = multer({
-    dest:'images',
-    limits:{
-        fileSize:1000000
-    },
-    fileFilter(req,file,cb){
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('please upload a word document'))
-        }
-        cb(undefined,true)
+// const multer = require('multer')
+// const upload = multer({
+//     dest:'images',
+//     limits:{
+//         fileSize:1000000
+//     },
+//     fileFilter(req,file,cb){
+//         if(!file.originalname.match(/\.(doc|docx)$/)){
+//             return cb(new Error('please upload a word document'))
+//         }
+//         cb(undefined,true)
 
 
 
-        // cb(new Error('File must be a PDF'))
-        // cb(undefined,true)
-        // cb(undefined,false)
-    }
-})
+//         // cb(new Error('File must be a PDF'))
+//         // cb(undefined,true)
+//         // cb(undefined,false)
+//     }
+// })
 
-// const errorMiddleware = (req,res,next)=>{
-//     throw new Error('From my middleware')
-// }
+// // const errorMiddleware = (req,res,next)=>{
+// //     throw new Error('From my middleware')
+// // }
 
-app.post('/upload',upload.single('upload'),(req,res)=>{
-    res.send()
-},(error,req,res,next)=>{
-    res.status(400).send({error:error.message})
-})
+// app.post('/upload',upload.single('upload'),(req,res)=>{
+//     res.send()
+// },(error,req,res,next)=>{
+//     res.status(400).send({error:error.message})
+// })
 
 app.use(express.json())
 app.use(userrouter)
